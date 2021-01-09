7bb64238b5610be2fe33af12cb2f44f12cc8ce70
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-09 19:18:11 +0900

    rubocopの導入・実行

--- test/controllers/cookings_controller_test.rb
+++ test/controllers/cookings_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class CookingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get cookings_index_url
     assert_response :success
   end
-
 end
