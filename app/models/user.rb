c96e3ac317feba624c2a59072f61b68a4dbcc8d9
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-30 11:46:30 +0900

    新規・ログイン画面のエラ〜メッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ApplicationRecord
         validates :nickname 
         validates :password_confirmation
         end
-        validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}\z/i }, confirmation: true      
+        validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}\z/i }, confirmation: true  
+        validates :email, uniqueness: true    
 end
