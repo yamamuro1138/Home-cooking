9134123692ff32d91ef3a5a768885e3e425f3da0
yamamurodaisuke <daisuke0308@icloud.com>
2021-02-01 15:19:38 +0900

    userモデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,7 @@ class User < ApplicationRecord
 
       has_many :cookings
 
-      with_options presence: true do
-        validates :nickname 
-        validates :password_confirmation
-      end
-        validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}\z/i }, confirmation: true  
-        validates :email, uniqueness: true
+      validates :nickname, presence: true
+      validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}\z/i }, confirmation: true
+      validates :email, uniqueness: true
 end
