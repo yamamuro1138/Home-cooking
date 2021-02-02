b5ed2b143dded3c8aca3661bda69bb0c3a716bdd
yamamurodaisuke <daisuke0308@icloud.com>
2021-02-02 15:42:14 +0900

    update: cookingsテーブル作成

--- db/migrate/20210202061019_create_cookings.rb
+++ db/migrate/20210202061019_create_cookings.rb
@@ -3,6 +3,11 @@ class CreateCookings < ActiveRecord::Migration[6.0]
     create_table :cookings do |t|
 
       t.timestamps
+      t.string :name,           null: false
+      t.string :recipe,         null: false
+      t.string :point,          null: false
+      t.string :material,       null: false
+      t.references :user,       foreign_key: true
     end
   end
 end
