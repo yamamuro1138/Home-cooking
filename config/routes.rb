3a7efe7b93b703681544ff5a8e9782a374c6809b
yamamurodaisuke <daisuke0308@icloud.com>
2021-02-05 11:30:18 +0900

    createアクションの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cookings#index'
-  resources :cookings, only: [:new, :edit, :show, :index]
+  resources :cookings, only: [:new, :edit, :show, :index, :create]
 end
