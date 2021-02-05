67cf99d24f63169156381a793554c89768b12711
yamamurodaisuke <daisuke0308@icloud.com>
2021-02-05 11:29:21 +0900

    cookingクラスのインスタンス変数を生成

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -1,9 +1,12 @@
 class CookingsController < ApplicationController
+  before_action :cooking_find, only: [:show, :edit, :update, :destroy]
+
   def index
     @cookings = Cooking.all
   end
 
   def new
+    @cooking = Cooking.new
   end
 
   def edit
@@ -16,5 +19,9 @@ class CookingsController < ApplicationController
   
   def message_params
     params.require(:cooking).permit(:name, :recipe, :point, :material, :image).merge(user_id: current_user.id)
+  end
+
+  def cooking_find
+    @cooking = Cooking.find(params[:id])
   end
 end
